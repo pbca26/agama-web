e82edb9d6be3dc729a10c9503f0af06306165679
pbca26 <pbca26@gmail.com>
2018-05-21 23:05:20 +0300

    fix app support links

--- src/components/dashboard/support/support.js
+++ src/components/dashboard/support/support.js
@@ -6,25 +6,6 @@ class Support extends React.Component {
     super();
   }
 
-  openExternalWindow(url) {
-    const remote = window.require('electron').remote;
-    const BrowserWindow = remote.BrowserWindow;
-
-    const externalWindow = new BrowserWindow({
-      width: 1280,
-      height: 800,
-      title: `${translate('INDEX.LOADING')}...`,
-      icon: remote.getCurrentWindow().iguanaIcon,
-    });
-
-    externalWindow.loadURL(url);
-    externalWindow.webContents.on('did-finish-load', () => {
-      setTimeout(() => {
-        externalWindow.show();
-      }, 40);
-    });
-  }
-
   render() {
     return (
       <div className="page margin-left-0">
@@ -33,58 +14,58 @@ class Support extends React.Component {
           <div className="row">
             <div className="col-sm-12 no-padding-left">
               <div className="support-box-wrapper">
-                <div
+                <a
                   className="support-box"
-                  onClick={ () => this.openExternalWindow('http://support.supernet.org') }>
+                  href="http://support.supernet.org">
                   <img
                     src="assets/images/cryptologo/supernet.png"
                     alt={ translate('SETTINGS.SUPPORT_TICKETS') } />
                   <div className="support-box-title">{ translate('SETTINGS.SUPPORT_TICKETS') }</div>
                   <div className="support-box-link">support.supernet.org</div>
-                </div>
+                </a>
               </div>
               <div className="support-box-wrapper">
-                <div
+                <a
                   className="support-box"
-                  onClick={ () => this.openExternalWindow('https://sprnt.slack.com') }>
+                  href="https://sprnt.slack.com">
                   <img
                     src="assets/images/support/slack-icon.png"
                     alt="Slack" />
                   <div className="support-box-title">Slack</div>
                   <div className="support-box-link">sprnt.slack.com</div>
-                </div>
+                </a>
               </div>
               <div className="support-box-wrapper">
-                <div
+                <a
                   className="support-box"
-                  onClick={ () => this.openExternalWindow('http://slackinvite.supernet.org/') }>
+                  href="http://slackinvite.supernet.org">
                   <img
                     src="assets/images/support/slack-invite-icon.png"
                     alt={ translate('SETTINGS.GET_SLACK_INVITE') } />
                   <div className="support-box-title">{ translate('SETTINGS.GET_SLACK_INVITE') }</div>
                   <div className="support-box-link">slackinvite.supernet.org</div>
-                </div>
+                </a>
               </div>
               <div className="support-box-wrapper">
-                <div
+                <a
                   className="support-box"
-                  onClick={ () => this.openExternalWindow('https://github.com/KomodoPlatform/Agama') }>
+                  href="https://github.com/KomodoPlatform/Agama">
                   <img
                     src="assets/images/support/github-icon.png"
                     alt="Github" />
                   <div className="support-box-title">Github</div>
                   <div className="support-box-link">github.com/KomodoPlatform/Agama</div>
-                </div>
+                </a>
               </div>
             </div>
           </div>
           <div className="row margin-top-30">
             <div className="col-sm-12">
               <p>
-                For guides & FAQ please go to <a onClick={ () => this.openExternalWindow('https://support.komodoplatform.com/support/home') }>https://support.komodoplatform.com/support/home</a>
+                For guides & FAQ please go to <a className="pointer" onClick={ () => this.openExternalWindow('https://support.komodoplatform.com/support/home') }>https://support.komodoplatform.com/support/home</a>
               </p>
               <p>
-                To send feedback please open a ticket at <a onClick={ () => this.openExternalWindow('https://support.komodoplatform.com/support/tickets/new') }>https://support.komodoplatform.com/support/tickets/new</a>
+                To send feedback please open a ticket at <a className="pointer" onClick={ () => this.openExternalWindow('https://support.komodoplatform.com/support/tickets/new') }>https://support.komodoplatform.com/support/tickets/new</a>
               </p>
             </div>
           </div>
