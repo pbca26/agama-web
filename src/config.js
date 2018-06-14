459f973b7a62a670c098f0707114da4f1f04051e
pbca26 <pbca26@gmail.com>
2018-06-14 14:47:32 +0300

    example

--- src/config.js
+++ src/config.js
@@ -5,7 +5,16 @@ let Config = {
   defaultLang: 'EN',
   roundValues: false,
   // single coin option
-  whitelabel: false,
+  whitelabel: true,
+  wlConfig: {
+    title: 'Komodo web wallet', // app title
+    mainLogo: 'native/kmd_header_title_logo.png', // login logo
+    coin: {
+      ticker: 'KMD',
+      name: 'Komodo',
+      logo: 'cryptologo/kmd.png', // dashboard coin logo
+    },
+  },
 };
 
 export default Config;
